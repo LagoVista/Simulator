73355d7e440fd5e918739cdd63e64efd22fff90a
Kevin D Wolf <kevinw@software-logistics.com>
2017-04-03 07:56:29 -0400

    added views

---
+++ src/LagoVista.Simulator/ViewModels/Simulator/SimulatorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LagoVista.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Simulator.ViewModels.Simulator
+{
+    public class SimulatorViewModel : ViewModelBase
+    {
+    }
+}
---
+++ src/LagoVista.Simulator/Views/Simulator/SimulatorsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using LagoVista.XPlat.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LagoVista.Simulator.Views.Simulator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SimulatorsView : LagoVistaContentPage
+    {
+        public SimulatorsView()
+        {
+            InitializeComponent();
+        }
+       
+    }    
+}
