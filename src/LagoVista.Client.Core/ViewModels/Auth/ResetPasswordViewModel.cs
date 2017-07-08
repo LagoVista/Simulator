2972c3b3843ba67eeb71f8333f939dd089c02fbb
Kevin D. Wolf <kevinw@software-logistics.com>
2017-07-08 19:07:47 -0400

    add views for reset/change pwd

---
+++ src/LagoVista.Client.Core/ViewModels/Auth/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using LagoVista.Core.Commanding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Client.Core.ViewModels.Auth
+{
+    public class ResetPasswordViewModel : IoTAppViewModelBase
+    {
+
+        private String _emailAddress;
+
+        public String EmailAddress
+        {
+            get { return _emailAddress; }
+            set { Set(ref _emailAddress, value); }
+        }
+
+        public RelayCommand SendReetPasswordLink { get; set; }
+
+    }
+}
--- src/LagoVista.Client.Core/ViewModels/Auth/SendResetPasswordLinkViewModel.cs
+++ src/LagoVista.Client.Core/ViewModels/Auth/SendResetPasswordLinkViewModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LagoVista.Client.Core.ViewModels.Auth
 {
-    public class ForgotPasswordViewModel
+    class SendResetPasswordLinkViewModel
     {
     }
 }
