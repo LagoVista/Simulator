a822bf4adb06cd71367edc3a2bfc614ae85f074b
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-07-16 10:07:47 -0400

    add stubs for views

---
+++ src/LagoVista.DeviceManager.Core/ViewModels/ManageDeviceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.DeviceManager.Core.ViewModels
+{
+    public class ManageDeviceViewModel
+    {
+    }
+}
---
+++ src/LagoVista.DeviceManager/LagoVista.DeviceManager/Views/ProvisionDeviceView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LagoVista.XPlat.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LagoVista.DeviceManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProvisionDeviceView : LagoVistaContentPage
+    {
+        public ProvisionDeviceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
